752c713d83f64af69bec56cd97b40f815b5a54a5
Serhii Stanislav <serhiistanislav@gmail.com>
2024-11-27 18:11:06 +0200

    EDIT | Error logic

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,16 +1,33 @@
 import { redirect } from 'next/navigation';
 import { checkIsAuthenticated } from '@/src/lib/auth/checkIsAuthedServerAction';
-import ArticlePage from '@/src/app/articles/[id]/article';
+// import ArticlePage from '@/src/app/articles/[id]/article';
+import { Article } from '@/src/lib/mongoose/models/Article';
 
 type Props = { params: Promise<{ id: string }> };
 
 const Articles = async ({ params }: Props) => {
   const isAuthenticated = await checkIsAuthenticated();
 
+  const { id } = await params;
+
+  const article = await Article.findById(id).exec(); // 67461eca11d202070efa0c33
+
   if (!isAuthenticated) {
     redirect('/auth/sign-in');
   } else {
-    return <ArticlePage params={params} />;
+    // return <ArticlePage params={params} />;
+    return (
+      <div>
+        <h1>{article.title}</h1>
+        <p>{article.content}</p>
+        <p>
+          <small>
+            Created on: {new Date(article.createdAt).toLocaleDateString()} |
+            Updated on: {new Date(article.updatedAt).toLocaleDateString()}
+          </small>
+        </p>
+      </div>
+    );
   }
 };
 
