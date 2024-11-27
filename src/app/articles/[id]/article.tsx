d9dcb4d2427e444e2381b8a7c735aed16bc8ee20
Serhii Stanislav <serhiistanislav@gmail.com>
2024-11-27 19:10:07 +0200

    EDIT | Error logic

--- src/app/articles/[id]/article.tsx
+++ src/app/articles/[id]/article.tsx
@@ -1,9 +1,9 @@
 import { Article } from '@/src/lib/mongoose/models/Article';
+import { redirect } from 'next/navigation';
 import mongoose from 'mongoose';
 
 type Props = { params: Promise<{ id: string }> };
 
-// /* remove later
 // Fetch the article on the server side
 const getArticleById = async (id: string) => {
   if (!mongoose.connection.readyState) {
@@ -11,30 +11,38 @@ const getArticleById = async (id: string) => {
   }
   return await Article.findById(id).exec();
 };
-// */
 
 const ArticlePage = async ({ params }: Props) => {
   const { id } = await params;
 
-  const article = await getArticleById(id); // 67461eca11d202070efa0c33
-  // const article = await Article.findById(id).exec(); // 67461eca11d202070efa0c33
+  try {
+    const article = await getArticleById(id); // 67461eca11d202070efa0c33
 
-  if (!article) {
-    return <div>Article not found 2</div>;
+    // if (!article) {
+    //   return <div>Article not found 2</div>;
+    // }
+
+    if (!article) {
+      throw new Error('Article not found');
+    }
+
+    return (
+      <div>
+        <h1>{article.title}</h1>
+        <p>{article.content}</p>
+        <p>
+          <small>
+            Created on: {new Date(article.createdAt).toLocaleDateString()} |
+            Updated on: {new Date(article.updatedAt).toLocaleDateString()}
+          </small>
+        </p>
+      </div>
+    );
+  } catch (error) {
+    console.error(`Error fetching article: ${error}`);
+    // Redirect to error page
+    redirect(`/articles/${id}/error`);
   }
-
-  return (
-    <div>
-      <h1>{article.title}</h1>
-      <p>{article.content}</p>
-      <p>
-        <small>
-          Created on: {new Date(article.createdAt).toLocaleDateString()} |
-          Updated on: {new Date(article.updatedAt).toLocaleDateString()}
-        </small>
-      </p>
-    </div>
-  );
 };
 
 export default ArticlePage;
