9559f20f5dc5408df4ba7699582259c5c4d9e106
Serhii Stanislav <serhiistanislav@gmail.com>
2024-11-29 18:54:50 +0200

    EDIt | fb comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import Providers from '@/src/providers';
+import FacebookSDKLoader from '../components/Layout/FacebookSDKLoader';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: Readonly<ChildrenProps>) {
 
       <Providers>
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          <FacebookSDKLoader />
           {children}
         </body>
       </Providers>
