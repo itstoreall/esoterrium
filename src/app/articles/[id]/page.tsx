f3252a63bf86705ea5c53e65370b3577b42aef88
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-03 19:23:17 +0200

    ADD | test errors 14

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
-// import jsonParse from '@/src/utils/jsonParse';
+import jsonParse from '@/src/utils/jsonParse';
 import metadataHandler from '@/src/utils/metadataHandler';
 import { roleAccess } from '@/src/lib/auth/roleAccessServerAction';
 import { checkIsAuthenticated } from '@/src/lib/auth/checkIsAuthedServerAction';
@@ -42,27 +42,27 @@ const Article = async ({ params }: Props) => {
   const article = await getArticle(id);
 
   if (article.access === 'public') {
-    const d = {
-      _id: '67719da6bfd356c5bf540700',
-      idx: 3,
-      title: 'Шесть миров',
-      content: 'Шесть миров бытия',
-      image:
-        'https://res.cloudinary.com/dsxdnz1hq/image/upload/v1744298123/lokas_r6srkx.jpg',
-      author: 'Esoterrium',
-      tags: ['Просветление'],
-      views: ['cm4mu7gw90000bkpyolhloi9k'],
-      likes: [],
-      isPublished: true,
-      access: 'public',
-      publishedAt: new Date(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    return <ArticleDetailPage article={d} />;
+    // const d = {
+    //   _id: '67719da6bfd356c5bf540700',
+    //   idx: 3,
+    //   title: 'Шесть миров',
+    //   content: 'Шесть миров бытия',
+    //   image:
+    //     'https://res.cloudinary.com/dsxdnz1hq/image/upload/v1744298123/lokas_r6srkx.jpg',
+    //   author: 'Esoterrium',
+    //   tags: ['Просветление'],
+    //   views: ['cm4mu7gw90000bkpyolhloi9k'],
+    //   likes: [],
+    //   isPublished: true,
+    //   access: 'public',
+    //   publishedAt: new Date(),
+    //   createdAt: new Date(),
+    //   updatedAt: new Date(),
+    // };
+    // return <ArticleDetailPage article={d} />;
     // return <ArticleDetailPage article={article} />;
     // return <div style={{ color: 'red' }}>Details 1 {article._id}</div>;
-    // return <ArticleDetailPage article={jsonParse(article)} />;
+    return <ArticleDetailPage article={jsonParse(article)} />;
   } else {
     const access = await roleAccess('article-details');
     if (!access.isAccess) {
@@ -76,26 +76,26 @@ const Article = async ({ params }: Props) => {
       if (!isAuthenticated) {
         redirect('/auth/sign-in');
       } else {
-        const d = {
-          _id: '67719da6bfd356c5bf540700',
-          idx: 3,
-          title: 'Шесть миров',
-          content: 'Шесть миров бытия',
-          image:
-            'https://res.cloudinary.com/dsxdnz1hq/image/upload/v1744298123/lokas_r6srkx.jpg',
-          author: 'Esoterrium',
-          tags: ['Просветление'],
-          views: ['cm4mu7gw90000bkpyolhloi9k'],
-          likes: [],
-          isPublished: true,
-          access: 'public',
-          publishedAt: new Date(),
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        };
-        return <ArticleDetailPage article={d} />;
+        // const d = {
+        //   _id: '67719da6bfd356c5bf540700',
+        //   idx: 3,
+        //   title: 'Шесть миров',
+        //   content: 'Шесть миров бытия',
+        //   image:
+        //     'https://res.cloudinary.com/dsxdnz1hq/image/upload/v1744298123/lokas_r6srkx.jpg',
+        //   author: 'Esoterrium',
+        //   tags: ['Просветление'],
+        //   views: ['cm4mu7gw90000bkpyolhloi9k'],
+        //   likes: [],
+        //   isPublished: true,
+        //   access: 'public',
+        //   publishedAt: new Date(),
+        //   createdAt: new Date(),
+        //   updatedAt: new Date(),
+        // };
+        // return <ArticleDetailPage article={d} />;
         // return <div style={{ color: 'red' }}>Details 2 {article._id}</div>;
-        // return <ArticleDetailPage article={jsonParse(article)} />;
+        return <ArticleDetailPage article={jsonParse(article)} />;
       }
     } catch (error) {
       console.error(`Error generating metadata: ${error}`);
