410a5844a7e1857db72365f1da3fb2d7c130d5fa
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-03 21:07:47 +0200

    ADD | test errors 25

--- src/components/MDEditorBlock.tsx
+++ src/components/MDEditorBlock.tsx
@@ -6,18 +6,13 @@ type Props = {
 };
 
 const MDEditorBlock = ({ className, text }: Props) => {
-  // console.log('text:', text);
-
   return (
     <div className={`mdeditor-block ${className}`}>
-      {text && (
-        <MDEditor.Markdown
-          style={{ whiteSpace: 'pre-wrap' }}
-          className="mdeditor-markdown-top-lib-elenemt"
-          source={'text 111'}
-          // source={text}
-        />
-      )}
+      <MDEditor.Markdown
+        style={{ whiteSpace: 'pre-wrap' }}
+        className="mdeditor-markdown-top-lib-elenemt"
+        source={text}
+      />
     </div>
   );
 };
