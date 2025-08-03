c6e1d0ab7fd390f0157ab00f6db656ec63852c57
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-03 19:14:36 +0200

    ADD | test errors 12

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,10 +1,10 @@
 import { redirect } from 'next/navigation';
+// import jsonParse from '@/src/utils/jsonParse';
 import metadataHandler from '@/src/utils/metadataHandler';
 import { roleAccess } from '@/src/lib/auth/roleAccessServerAction';
 import { checkIsAuthenticated } from '@/src/lib/auth/checkIsAuthedServerAction';
 import { getArticleById } from '@/src/lib/mongoose/getArticleByIdServerAction';
-// import ArticleDetailPage from '@/src/app/articles/[id]/article-detail';
-// import jsonParse from '@/src/utils/jsonParse';
+import ArticleDetailPage from '@/src/app/articles/[id]/article-detail';
 
 type Props = { params: Promise<{ id: string }> };
 
@@ -38,37 +38,6 @@ export async function generateMetadata({ params }: Props) {
 }
 
 const Article = async ({ params }: Props) => {
-  /*
-  const { id } = await params;
-  const article = await getArticle(id);
-  console.log('article:', article);
-
-  if (article.access === 'public') {
-    const d = {
-      _id: '67719da6bfd356c5bf540700',
-      idx: 3,
-      title: 'Шесть миров',
-      content: 'Шесть миров бытия',
-      image:
-        'https://res.cloudinary.com/dsxdnz1hq/image/upload/v1744298123/lokas_r6srkx.jpg',
-      author: 'Esoterrium',
-      tags: ['Просветление'],
-      views: ['cm4mu7gw90000bkpyolhloi9k'],
-      likes: [],
-      isPublished: true,
-      access: 'public',
-      publishedAt: new Date(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    // return <ArticleDetailPage article={d} />;
-    // return <ArticleDetailPage article={jsonParse(article)} />;
-    return <div style={{ color: 'red' }}>Details 1 {d._id}</div>;
-  } else {
-    return <div style={{ color: 'red' }}>Details 2</div>;
-  }
-  */
-
   const { id } = await params;
   const article = await getArticle(id);
 
@@ -91,8 +60,8 @@ const Article = async ({ params }: Props) => {
     //   updatedAt: new Date(),
     // };
     // return <ArticleDetailPage article={d} />;
-    // return <ArticleDetailPage article={jsonParse(article)} />;
-    return <div style={{ color: 'red' }}>Details 1 {article._id}</div>;
+    return <ArticleDetailPage article={article} />;
+    // return <div style={{ color: 'red' }}>Details 1 {article._id}</div>;
     // return <ArticleDetailPage article={jsonParse(article)} />;
   } else {
     const access = await roleAccess('article-details');
@@ -124,8 +93,8 @@ const Article = async ({ params }: Props) => {
         //   createdAt: new Date(),
         //   updatedAt: new Date(),
         // };
-        // return <ArticleDetailPage article={d} />;
-        return <div style={{ color: 'red' }}>Details 2 {article._id}</div>;
+        return <ArticleDetailPage article={article} />;
+        // return <div style={{ color: 'red' }}>Details 2 {article._id}</div>;
         // return <ArticleDetailPage article={jsonParse(article)} />;
       }
     } catch (error) {
