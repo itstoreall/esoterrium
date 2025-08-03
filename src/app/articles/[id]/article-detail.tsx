18746acb0a5307d51638d9960a11d2a6245e254f
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-03 19:19:39 +0200

    ADD | test errors 13

--- src/app/articles/[id]/article-detail.tsx
+++ src/app/articles/[id]/article-detail.tsx
@@ -1,12 +1,15 @@
 import { ArticleData } from '@/src/types';
 import Container from '@/src/components/Container';
-import ArticleDetails from '@/src/components/Page/ArticleDetails';
+// import ArticleDetails from '@/src/components/Page/ArticleDetails';
 
 const ArticleDetailPage = async ({ article }: { article: ArticleData }) => {
   return (
     <Container className={'page-wrapper-container'}>
       <Container className={'main-aside-combine-wrapper-container'}>
-        <ArticleDetails article={article} />
+        <div style={{ color: 'teal', paddingTop: '100px' }}>
+          Details 3 {article.title}
+        </div>
+        {/* <ArticleDetails article={article} /> */}
       </Container>
     </Container>
   );
