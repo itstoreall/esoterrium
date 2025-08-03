8e7e76fb6635f4e530e160e09bc979d4af023ace
Serhii Stanislav <serhiistanislav@gmail.com>
2025-08-03 19:55:13 +0200

    ADD | test errors 16

--- src/components/ArticleDetailsContent.tsx
+++ src/components/ArticleDetailsContent.tsx
@@ -1,85 +1,93 @@
 'use client';
 
-import Image from 'next/image';
+// import Image from 'next/image';
 import useUserRole from '@/src/hooks/useUserRole';
-import useLoader from '@/src/hooks/useLoader';
-import { config } from '@/src/config';
+// import useLoader from '@/src/hooks/useLoader';
+// import { config } from '@/src/config';
 import { ArticleData } from '@/src/types';
-import trimLongWord from '@/src/utils/trimLongWord';
-import ArticleDetailsPublicationInfo from '@/src/components/ArticleDetailsPublicationInfo';
-import AdminPanelArticleDetails from '@/src/components/AdminPanelArticleDetails';
-import ArticleDetailsComments from '@/src/components/Comments';
-import MDEditorBlock from '@/src/components/MDEditorBlock';
-import Sidebar from '@/src/components/Layout/Sidebar';
-import Container from '@/src/components/Container';
-import Title from '@/src/components/Layout/Title';
-import Main from '@/src/components/Layout/Main';
-import Section from '@/src/components/Section';
+// import trimLongWord from '@/src/utils/trimLongWord';
+// import ArticleDetailsPublicationInfo from '@/src/components/ArticleDetailsPublicationInfo';
+// import AdminPanelArticleDetails from '@/src/components/AdminPanelArticleDetails';
+// import ArticleDetailsComments from '@/src/components/Comments';
+// import MDEditorBlock from '@/src/components/MDEditorBlock';
+// import Sidebar from '@/src/components/Layout/Sidebar';
+// import Container from '@/src/components/Container';
+// import Title from '@/src/components/Layout/Title';
+// import Main from '@/src/components/Layout/Main';
+// import Section from '@/src/components/Section';
 
 const ArticleDetailsContent = ({ article }: { article: ArticleData }) => {
-  const { isLoader, Loader } = useLoader();
+  // const { isLoader, Loader } = useLoader();
   const acc = useUserRole();
 
   const isPublic = article.access === 'public';
 
+  console.log('acc:', acc);
+
   return (
-    <Container className="main-aside-combine-container">
-      <Sidebar className="article-details-sidebar" />
-
-      <Main className={'article-details-page-main'}>
-        {(!isPublic && !acc.userRole) || isLoader ? (
-          <>
-            <Section className={'main-heading-section article-details-heading'}>
-              <Title tag="h2" className="page-main-title" text={'...'} />
-            </Section>
-            <Loader className="light-loader-block" />
-          </>
-        ) : (
-          <>
-            {/* <Section className={'main-heading-section'}>{null}</Section> */}
-
-            <Section className="article-details-section">
-              <Title
-                className="article-details-title readable-content "
-                text={trimLongWord(article.title, 20, 15, 5)}
-              />
-
-              <ArticleDetailsPublicationInfo article={article} />
-
-              {acc.isAdminRole() && (
-                <AdminPanelArticleDetails article={article} />
-              )}
-
-              <div className="image-block article-details-thumb">
-                <Image
-                  src={article.image || config.defaultImageUrl}
-                  className={'article-details-image'}
-                  fill
-                  priority={true}
-                  alt={article.title}
-                />
-              </div>
-
-              <MDEditorBlock
-                className="article-details-mdeditor-block"
-                text={article.content}
-              />
-
-              {/* <p className="article-details-text readable-content">
-                {article.content}
-              </p> */}
-            </Section>
-
-            <span className="articles-page-main-divider" />
-
-            <ArticleDetailsComments articleId={article._id} />
-
-            {/* <Section className={'main-final-section'}>{null}</Section> */}
-          </>
-        )}
-      </Main>
-    </Container>
+    <div style={{ color: 'tomato', paddingTop: '100px' }}>
+      Details 4 {`${article.title} - ${isPublic}`}
+    </div>
   );
+
+  // return (
+  //   <Container className="main-aside-combine-container">
+  //     <Sidebar className="article-details-sidebar" />
+
+  //     <Main className={'article-details-page-main'}>
+  //       {(!isPublic && !acc.userRole) || isLoader ? (
+  //         <>
+  //           <Section className={'main-heading-section article-details-heading'}>
+  //             <Title tag="h2" className="page-main-title" text={'...'} />
+  //           </Section>
+  //           <Loader className="light-loader-block" />
+  //         </>
+  //       ) : (
+  //         <>
+  //           {/* <Section className={'main-heading-section'}>{null}</Section> */}
+
+  //           <Section className="article-details-section">
+  //             <Title
+  //               className="article-details-title readable-content "
+  //               text={trimLongWord(article.title, 20, 15, 5)}
+  //             />
+
+  //             <ArticleDetailsPublicationInfo article={article} />
+
+  //             {acc.isAdminRole() && (
+  //               <AdminPanelArticleDetails article={article} />
+  //             )}
+
+  //             <div className="image-block article-details-thumb">
+  //               <Image
+  //                 src={article.image || config.defaultImageUrl}
+  //                 className={'article-details-image'}
+  //                 fill
+  //                 priority={true}
+  //                 alt={article.title}
+  //               />
+  //             </div>
+
+  //             <MDEditorBlock
+  //               className="article-details-mdeditor-block"
+  //               text={article.content}
+  //             />
+
+  //             {/* <p className="article-details-text readable-content">
+  //               {article.content}
+  //             </p> */}
+  //           </Section>
+
+  //           <span className="articles-page-main-divider" />
+
+  //           <ArticleDetailsComments articleId={article._id} />
+
+  //           {/* <Section className={'main-final-section'}>{null}</Section> */}
+  //         </>
+  //       )}
+  //     </Main>
+  //   </Container>
+  // );
 };
 
 export default ArticleDetailsContent;
--- src/components/Page/ArticleDetails.tsx
+++ src/components/Page/ArticleDetails.tsx
@@ -1,11 +1,8 @@
 import { ArticleData } from '@/src/types';
-// import ArticleDetailsContent from '@/src/components/ArticleDetailsContent';
+import ArticleDetailsContent from '@/src/components/ArticleDetailsContent';
 
 const ArticleDetails = ({ article }: { article: ArticleData }) => (
-  <div style={{ color: 'teal', paddingTop: '100px' }}>
-    Details 4 {article.author}
-  </div>
-  // <ArticleDetailsContent article={article} />
+  <ArticleDetailsContent article={article} />
 );
 
 export default ArticleDetails;
